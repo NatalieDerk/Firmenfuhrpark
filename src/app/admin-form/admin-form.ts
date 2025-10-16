5a49c54010c88daccd57e00c91449523e9b89f52
Derk <Natalie.Derk@inros-lackner.de>
2025-10-16 08:22:23 +0200

    Initial commit

---
+++ src/app/admin-form/admin-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-form',
+  standalone: false,
+  templateUrl: './admin-form.html',
+  styleUrl: './admin-form.css'
+})
+export class AdminForm {
+
+}
