87906e1301907d9075316f5b4c0ae9bf44a43815
DerkNatalie <DerkNatalia87@gmail.com>
2025-12-08 17:54:19 +0100

    update backend

--- Fuhrpark.Tests/obj/Debug/net8.0/Fuhrpark.Tests.AssemblyInfo.cs
+++ Fuhrpark.Tests/obj/Debug/net8.0/Fuhrpark.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fuhrpark.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+491ef42aa726917b80c30cdefd13b37e55e5ea41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4329b98f5be6cb2fa65a293be75f1b1cf15a248d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fuhrpark.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fuhrpark.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
