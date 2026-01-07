9d13a360cca9a4c5acf8c04b3cc112ec6569b7b9
DerkNatalie <DerkNatalia87@gmail.com>
2026-01-07 08:10:41 +0100

    updade Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDBContext>
 (options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options => 
+    { options.AddDefaultPolicy("AllowAngularDev", policy => 
+        { policy.WithOrigins("http://localhost:4200") 
+        .AllowAnyHeader() 
+        .AllowAnyMethod() 
+        .AllowCredentials(); 
+        }); 
+    }); 
+
 // Controller hinzufügen
-builder.Services.AddControllers()
-    .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
+builder.Services.AddControllers() .AddJsonOptions(options => 
+    { options.JsonSerializerOptions.PropertyNamingPolicy = null; });
 
 // Swagger-Dinste hinzufügen
 builder.Services.AddEndpointsApiExplorer();
@@ -34,5 +43,21 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<ApplicationDBContext>();
 }
-app.MapControllers();
+
+builder.Services.AddControllers()
+.AddJsonOptions(options => { options.JsonSerializerOptions.PropertyNamingPolicy = null; });
+
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "dist";
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAngularDev"); 
+app.UseStaticFiles(); 
+app.Use.SpaStaticFiles(); 
+app.MapControllers(); 
+app.MapFallbackToFile("index.html"); 
+app.MapHub<BookingHub>; 
 app.Run();
